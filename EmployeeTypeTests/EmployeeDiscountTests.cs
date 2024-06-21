cd7f9636ccca990ea18ba68a693fd8bb89a606a3
harsimran-litt <canharsimran03@gmail.com>
2024-06-21 14:31:30 -0400

    created unit tests cases for tests 4 passed 2 failes

---
+++ EmployeeTypeTests/EmployeeDiscountTests.cs
@@ -0,0 +1,101 @@
+﻿using LaptopDiscount;
+
+namespace EmployeeTypeTests
+{
+
+
+    [TestFixture]
+    public class EmployeeDiscountTests
+    {
+        [Test]
+        public void CalDiscountPrice_PartialLoadEmployee_5PercentDis()
+        {
+            var discount = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.PartialLoad,
+                Price = 200
+            };
+
+            decimal discountedPrice = discount.CalculateDiscountedPrice();
+
+            //Assert
+            Assert.AreEqual(190, discountedPrice); // 5% discount is expected
+        }
+
+
+
+        [Test]
+        public void CalcDisPrice_FullTime_Employee_10Per_Dis()
+        {
+            var discount = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.FullTime,
+                Price = 150
+            };
+
+            decimal discountedPrice = discount.CalculateDiscountedPrice();
+
+            //Assert
+            Assert.AreEqual(135, discountedPrice);
+        }
+
+
+        [Test]
+        public void CalcDiscPrice_CompanyPurchasing_20PerDis()
+        {
+            var discount = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.CompanyPurchasing,
+                Price = 200
+            };
+
+            decimal discountedPrice = discount.CalculateDiscountedPrice();
+            //Assert
+            Assert.AreEqual(160, discountedPrice); // 20% discount is expected
+        }
+
+        [Test]
+        public void CalculateDiscountedPrice_NegativeVale_Invalid()
+        {
+            var discount = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.FullTime,
+                Price = -10
+            };
+            //assign
+            decimal discountedPrice = discount.CalculateDiscountedPrice();
+            Assert.AreEqual(-10, discountedPrice); // No discount expected as the input is not valid
+        }
+
+
+        [Test]
+        public void CalculateDiscountedPrice_ExtremelyLowPrice_InvalidInput()
+        {
+
+            var discount = new EmployeeDiscount
+            {
+                EmployeeType = EmployeeType.CompanyPurchasing,
+                Price = 1
+            };
+
+            decimal discountedPrice = discount.CalculateDiscountedPrice();
+            //Assert
+            Assert.AreEqual(1, discountedPrice); // No discount expected because of invalid input as it is very low)
+        }
+
+
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- EmployeeTypeTests/UnitTest1.cs
+++ EmployeeTypeTests/UnitTest1.cs
@@ -1,7 +1,92 @@
+using LaptopDiscount;
+
 namespace EmployeeTypeTests
 {
-    public class Tests
-    {
+   
+    /*
+        [TestFixture]
+        public class EmployeeDiscountTests
+        {
+            [Test]
+            public void CalDiscountPrice_PartialLoadEmployee_5PercentDis()
+            {
+                var discount = new EmployeeDiscount
+                {
+                    EmployeeType = EmployeeType.PartialLoad,
+                    Price = 200 
+                };
+
+                decimal discountedPrice = discount.CalculateDiscountedPrice();
+
+                //Assert
+                Assert.AreEqual(190, discountedPrice); // 5% discount is expected
+            }
+
+
+
+            [Test]
+            public void CalcDisPrice_FullTime_Employee_10Per_Dis()
+            {
+                var discount = new EmployeeDiscount
+                {
+                    EmployeeType = EmployeeType.FullTime,
+                    Price = 150 
+                };
+
+                decimal discountedPrice = discount.CalculateDiscountedPrice();
+
+                //Assert
+                Assert.AreEqual(135, discountedPrice); 
+            }
+
+
+            [Test]
+            public void CalcDiscPrice_CompanyPurchasing_20PerDis()
+            {
+                var discount = new EmployeeDiscount
+                {
+                    EmployeeType = EmployeeType.CompanyPurchasing,
+                    Price = 200 
+                };
+
+                decimal discountedPrice = discount.CalculateDiscountedPrice();
+                //Assert
+                Assert.AreEqual(160, discountedPrice); // 20% discount is expected
+            }
+
+            [Test]
+            public void CalculateDiscountedPrice_NegativeVale_Invalid()
+            {
+                var discount = new EmployeeDiscount
+                {
+                    EmployeeType = EmployeeType.FullTime,
+                    Price = -10 
+                };
+                //assign
+                decimal discountedPrice = discount.CalculateDiscountedPrice();
+                Assert.AreEqual(-10, discountedPrice); // No discount expected as the input is not valid
+            }
+
+
+            [Test]
+            public void CalculateDiscountedPrice_ExtremelyLowPrice_InvalidInput()
+            {
+                
+                var discount = new EmployeeDiscount
+                {
+                    EmployeeType = EmployeeType.CompanyPurchasing,
+                    Price = 1 
+                };
+
+                decimal discountedPrice = discount.CalculateDiscountedPrice();
+                //Assert
+                Assert.AreEqual(1, discountedPrice); // No discount expected because of invalid input as it is very low)
+            }
+
+
+
+        
+
         [SetUp]
         public void Setup()
         {
@@ -12,5 +97,5 @@ namespace EmployeeTypeTests
         {
             Assert.Pass();
         }
-    }
+    }*/
 }
